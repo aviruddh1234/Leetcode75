4bede94d552fc5e8ccfe7ddb2bf47cbdf8a777e0
Aviruddh <aviruddhsinghal@gmail.com>
2025-06-18 11:19:30 +0530

    new questions

---
+++ practise/fact 1d dp.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1 
+    
+    dp = [0]*(n+1)
+    dp[0] = 1 
+    dp[1] = 1 
+    
+    for i in range(2,n+1):
+        dp[i] = i*dp[i-1]
+    
+    return dp[n]
+
+n = int(input())
+print(factorial(n))
