2c9d3b0a7c443c93c535b3695a6103207bf5c252
Aviruddh <aviruddhsinghal@gmail.com>
2025-06-26 23:07:21 +0530

    new question

---
+++ intervals/q72.py
@@ -0,0 +1,16 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        res = 0 
+
+        intervals.sort(key = lambda x: x[1])
+
+        prev = intervals[0][1]
+
+        for i in range(1, len(intervals)):
+            if prev > intervals[i][0]:
+                res += 1 
+            else:
+                prev = intervals[i][1]
+        
+        return res
+        
